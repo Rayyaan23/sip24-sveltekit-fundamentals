f2902724434863f3959c35819347bb4189d4c50e
Rayyaan23 <129207287+Rayyaan23@users.noreply.github.com>
2024-06-25 20:06:39 +0530

    files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,5 +20,5 @@
               </div>
           </div>
       {/each}
-   </div>
+  </div>
 </div>
--- src/routes/add-post/+page.server.js
+++ src/routes/add-post/+page.server.js
@@ -1,19 +1,21 @@
-import prisma from "$lib/prisma";
 import { redirect } from "@sveltejs/kit";
+import prisma from "$lib/prisma";
+
 export const actions = {
-    default: async ({ request })  => {
-        const data = Object.fromEntries(await request.formData());
+    default: async ({ request}) =>{
+        const data =  Object.fromEntries(await request.formData());
         let username = data.username;
-        let content = data.content;
+        let content =  data.content;
         let image = Buffer.from(await data.image.arrayBuffer()).toString('base64');
         
         await prisma.post.create({
             data: {
                 username,
                 content,
-                image
+                image,
             }
         });
-        throw redirect(302, "/");
-    }
+
+        throw redirect(302,"/");
+    }
 }
